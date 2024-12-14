f3537cb047caa28976b7be8f5eaa519e6229635f
Mohammad Danish <mddankhan5@gmail.com>
2024-12-14 17:15:05 +0530

    commit

--- springboot-blog-rest-api/src/main/java/com/springboot/blog/payload/CommentDto.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/payload/CommentDto.java
@@ -1,11 +1,23 @@
 package com.springboot.blog.payload;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CommentDto {
     private long id;
+
+    @NotEmpty(message = "name should not be empty")
+    @Size(min = 2)
     private String name;
+
+    @NotEmpty(message = "email should not be empty")
+    @Size(min = 8)
+    @Email
     private String email;
+
+    @NotEmpty(message = "name should not be empty")
     private  String body;
 }
